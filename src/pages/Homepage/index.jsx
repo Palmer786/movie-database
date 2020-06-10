51b1072ab7da52b4aca6bd42bae443e31e6da8a0
palmer786 <palmer786786@gmail.com>
2020-06-10 18:26:06 +0200

    initialize typescript

--- src/pages/Homepage/index.jsx
+++
@@ -1,7 +0,0 @@
-import React, {useEffect} from 'react';
-
-const Homepage = () => {
-  useEffect(() => (window.location = '/popular'));
-  return <div>{null}</div>;
-};
-export default Homepage;
---
+++ src/pages/Homepage/index.tsx
@@ -0,0 +1,10 @@
+import React, {useEffect} from 'react';
+
+const Homepage: React.FC = () => {
+  useEffect(() => {
+    window.location.href = '/popular';
+  });
+  return <div>{null}</div>;
+};
+
+export default Homepage;
